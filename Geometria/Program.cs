69a59304e0857a9149f7dadc5b6216daca76979b
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-18 17:23:51 +0200

    Add project files.

---
+++ Geometria/Program.cs
@@ -0,0 +1,52 @@
+﻿using rectangle;
+using System.Reflection.Metadata.Ecma335;
+
+//PRENDO I PARAMETRI DEL RETTANGOLO
+
+Console.WriteLine("Inserisci le dimensioni della base: ");
+int xDim = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci le dimensioni dell'altezza: ");
+int yDim = int.Parse(Console.ReadLine());
+
+Rectangle myRectangle = new Rectangle(xDim, yDim);
+
+//STAMPO LE INFO DEL RETTANGOLO
+
+Console.WriteLine();
+Console.WriteLine("-------------- INFO --------------");
+Console.WriteLine("La base è: " + myRectangle.getxDimension());
+Console.WriteLine("L'altezza è: " + myRectangle.getyDimension());
+Console.WriteLine("Il perimetro è: " + myRectangle.getPerimeter());
+Console.WriteLine("L'area è: " + myRectangle.getArea());
+Console.WriteLine("----------------------------------");
+Console.WriteLine();
+
+//STAMPO IL RETTANGOLO IN CONSOLE
+
+for (int i = 0; i < myRectangle.getyDimension(); i++)
+{
+    if (i == 0 || i == myRectangle.getyDimension() - 1)
+    {
+        for (int j = 0; j < myRectangle.getxDimension(); j++)
+        {
+            Console.Write("--");
+        }
+        Console.WriteLine();
+    } else
+    {
+        for (int j = 0; j < myRectangle.getxDimension(); j++)
+        {
+            if(j == 0)
+            {
+                Console.Write("| ");
+            } else if (j == myRectangle.getxDimension() - 1)
+            {
+                Console.Write(" |");
+            } else
+            {
+                Console.Write("  ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Geometria/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle
+{
+    public class Rectangle
+    {
+        private int xDimension;
+        private int yDimension;
+        private int area;
+        private int perimeter;
+        private int setArea(int xDimension, int yDimension)
+        {
+            return xDimension * yDimension;
+        }
+
+        private int setPerimeter(int xDimension, int yDimension)
+        {
+            return (xDimension * 2) + (yDimension * 2);
+        }
+
+        public Rectangle(int xDimension, int yDimension)
+        {
+            this.xDimension = xDimension;
+            this.yDimension = yDimension;   
+            this.area = setArea(xDimension, yDimension);
+            this.perimeter = setPerimeter(xDimension,yDimension);
+        }
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        public int getPerimeter()
+        {
+            return this.perimeter;
+        }
+
+        public int getxDimension()
+        {
+            return this.xDimension;
+        }
+
+        public int getyDimension()
+        {
+            return this.yDimension;
+        }
+    }
+}
